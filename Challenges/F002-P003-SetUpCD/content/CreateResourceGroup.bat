36fa10accb7c85440bec5577c41d20e9a897fa8b
Gourav Jain <gjain@xebia.com>
2018-06-12 18:47:43 +0530

    Changes

--- Challenges/F002-P003-SetUpCD/content/CreateResourceGroup.bat
+++ Challenges/F002-P003-SetUpCD/content/CreateResourceGroup.bat
@@ -1,8 +1,8 @@
 REM create resource group
-call az group create --name "xjg-rg-sampleapp" --location "West Europe"
+call az group create --name "xjg-rg-sampleapp-12-06" --location "West Europe"
 
 REM create appservice plan
-call az appservice plan create -g "xjg-rg-sampleapp" -n "xjg-app-sampleapp" --sku F1
+call az appservice plan create -g "xjg-rg-sampleapp-12-06" -n "xjg-app-sampleapp-12-06" --sku F1
 
 REM create azurewebapp 
-call az webapp create -g "xjg-rg-sampleapp" -p "xjg-app-sampleapp" -n "xjg-webapp-sampleapp"
+call az webapp create -g "xjg-rg-sampleapp-12-06" -p "xjg-app-sampleapp-12-06" -n "xjg-webapp-sampleapp-12-06"
--- Challenges/F002-P003-SetUpCD/content/DeleteResourceGroup.bat
+++ Challenges/F002-P003-SetUpCD/content/DeleteResourceGroup.bat
@@ -1,2 +1,2 @@
 REM delete resource group
-call az group delete --name "xjg-rg-sampleapp" --yes
+call az group delete --name "xjg-rg-sampleapp-12-06" --yes
