e31d774b095e3fe6d8f7d0d6ee6c0a4685c830ae
Gourav Jain <gjain@xebia.com>
2018-06-13 15:37:38 +0530

    Changes

--- Challenges/F003-P002/content/CreateAzureResources.cmd
+++ Challenges/F003-P002/content/CreateAzureResources.cmd
@@ -1,7 +1,6 @@
 SET Environment_name=%2
 SET ResourceGroup_name=%4
 
-
 REM create appservice plan
 call az appservice plan create -g "%ResourceGroup_name%" -n "AppServicePlan-TeamName-%Environment_name%" --sku F1
 
@@ -11,8 +10,16 @@ call az webapp create -g "%ResourceGroup_name%" -p "AppServicePlan-TeamName-%Env
 REM create SQL server
 call az sql server create --admin-password "Gourav@123" --admin-user "gourav8jain" --location "West Europe" --name "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
 
-REM set firewall rule to allow azure resources access the database
-az sql server firewall-rule create --name "azureservices" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0 --server "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
-
 REM create SQL database
 call az sql db create --name  "db-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%" --server "sqlserver-teamname-%Environment_name%"
+
+call az sql server update --resource-group "%ResourceGroup_name%" --name "sqlserver-TeamName-%Environment_name%" --admin-password "Gourav@123"
+
+REM Allow access to Azure services
+call az sql server firewall-rule create --resource-group "%ResourceGroup_name%" --server "sqlserver-teamname-%Environment_name%" --name "azureservices" --start-ip-address "0.0.0.0" --end-ip-address "0.0.0.0"
+
+REM set firewall rule to allow azure resources access the database
+az sql server firewall-rule create --name "azureservices-1" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0 --server "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
+
+
+
