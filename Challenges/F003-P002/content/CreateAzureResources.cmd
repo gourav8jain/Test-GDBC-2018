60ec33ad316eba99645aa25063cbf7bbf41b42f1
Gourav Jain <gjain@xebia.com>
2018-06-13 12:35:09 +0530

    Changes

--- Challenges/F003-P002/content/CreateAzureResources.cmd
+++ Challenges/F003-P002/content/CreateAzureResources.cmd
@@ -11,7 +11,7 @@ REM create SQL server
 call az sql server create --admin-password "Gourav@123" --admin-user "gourav8jain" --location "West Europe" --name "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
 
 REM set firewall rule to allow azure resources access the database
-az sql server firewall-rule create --name "azureservices" --start-ip-address 0.0.0.0 --end-ip-address 199.199.199.199 --server "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
+az sql server firewall-rule create --name "azureservices" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0 --server "sqlserver-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%"
 
 REM create SQL database
 call az sql db create --name  "db-teamname-%Environment_name%" --resource-group "%ResourceGroup_name%" --server "sqlserver-teamname-%Environment_name%"
